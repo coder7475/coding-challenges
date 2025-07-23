ede54e0246864422671935b1358bd0479b67d201
coder7475 <robiulhossain7475@gmail.com>
2025-07-23 11:50:32 +0600

    feat(wc): create mainfile

---
+++ wc-tool/main.cpp
@@ -0,0 +1,20 @@
+#include "wc.cpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cerr << "Usage: " << argv[0] << "<filename>\n";
+  }
+
+  string filename = argv[1];
+
+  WCResult result = countFromFile(filename);
+
+  cout << "Lines: " << result.lines << "\n";
+  cout << "Words: " << result.words << "\n";
+  cout << "Characters: " << result.characters << "\n";
+
+  return 0;
+}
