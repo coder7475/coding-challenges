07239bdc5a0eaecbe08ff767e99cb02a21abac7f
coder7475 <robiulhossain7475@gmail.com>
2025-07-26 12:03:40 +0600

    fix(wc): update output format to include byte count in default flag response

--- wc-tool/main.cpp
+++ wc-tool/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
   else if (flag == "-m")
     cout << result.characters << " " << filePath << "\n";
   else if (flag == "") 
-    cout << result.lines << " " << result.words << " " << result.characters << " " << filePath << "\n";
+    cout << "  " << result.lines << "  " << result.words << " " << result.bytes << " " << filePath << "\n";
   else {
     cerr << "Unknown flag: " << flag << "\n";
     return 1;
